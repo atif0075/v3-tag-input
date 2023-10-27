4f9d8f3118299c0e60a457b0f9a273fd04da29a1
M Atif <chatif476@gmail.com>
2023-10-28 01:55:13 +0500

    fixed: vue warning

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import V3TagInput from "./components/TagInput.vue";
+export { default as V3TagInput } from "./components/TagInput.vue";
 export default {
   install: (app, options) => {
     app.component("V3TagInput", V3TagInput);
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.js"),
